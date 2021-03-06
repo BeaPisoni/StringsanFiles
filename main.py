40cd32b5da833ef436d9b3bcdadac1113fd42ea3
bpbb <beatriz.pisoni.barba@gmail.com>
2021-03-06 14:32:14 +0100

    ciaciao

---
+++ main.py
@@ -0,0 +1,17 @@
+hi = "Hello"
+        print(hi[2])
+        print("Hello"[2])
+
+if hi [2] == hi[3]
+        print("This is true because we have double l")
+greeting = "hi" + " " + how + are + "you +""?"
+
+print(greeting)
+# Multiplication
+
+        print("hi > HI")
+else:
+        print (hi < Hi")
+print("1ello" > "hello")
+print("helloo"> "hello")
+print ("ana" in "banana")
